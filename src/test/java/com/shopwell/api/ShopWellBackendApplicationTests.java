88a353d8dc3e9a03db132e08ee16e5dfb92444a0
timolisa <timothyolisaeloka@gmail.com>
2023-06-15 22:03:29 +0100

    first commit

---
+++ src/test/java/com/shopwell/api/ShopWellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopwell.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopWellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
