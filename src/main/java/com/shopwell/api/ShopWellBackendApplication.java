69b0a1575436e0191f98640c7d1d46872ea6bd9b
timolisa <timothyolisaeloka@gmail.com>
2023-06-15 22:04:52 +0100

    first commit

--- src/main/java/com/shopwell/api/ShopWellBackendApplication.java
+++ src/main/java/com/shopwell/api/ShopWellBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopWellBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopWellBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopWellBackendApplication.class, args);
+    }
 
 }
