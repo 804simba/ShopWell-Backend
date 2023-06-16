61751e1c06e8c8d37ed7c8bf7449425f6ec28f58
timolisa <timothyolisaeloka@gmail.com>
2023-06-16 19:07:04 +0100

    add place order feature

--- src/main/java/com/shopwell/api/repository/OrderItemRepository.java
+++ src/main/java/com/shopwell/api/repository/OrderItemRepository.java
@@ -1,8 +1,9 @@
 package com.shopwell.api.repository;
 
+import com.shopwell.api.model.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
