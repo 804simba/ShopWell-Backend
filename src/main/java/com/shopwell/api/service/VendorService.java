ca06e4f3ec38d4a03e26d0ecf6fbaed5019845ec
timolisa <timothyolisaeloka@gmail.com>
2023-06-30 00:29:40 +0100

    delete employee and vendor

--- src/main/java/com/shopwell/api/service/VendorService.java
+++
@@ -1,8 +0,0 @@
-package com.shopwell.api.service;
-
-import com.shopwell.api.model.VOs.request.VendorRegistrationVO;
-
-public interface VendorService {
-
-    void registerVendor(VendorRegistrationVO registrationVO);
-}
