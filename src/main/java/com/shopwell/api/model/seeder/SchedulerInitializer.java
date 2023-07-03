a27ed48a1e0f385c9871e373ec52762659dcdee8
timolisa <timothyolisaeloka@gmail.com>
2023-07-03 18:38:18 +0100

    add roleEntity, roleDataInitializers, and schedulers

---
+++ src/main/java/com/shopwell/api/model/seeder/SchedulerInitializer.java
@@ -0,0 +1,17 @@
+package com.shopwell.api.model.seeder;
+
+import com.shopwell.api.service.BirthdayEmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SchedulerInitializer implements CommandLineRunner {
+    private final BirthdayEmailService birthdayEmailService;
+
+    @Override
+    public void run(String... args) {
+        birthdayEmailService.scheduleBirthdayEmailJob();
+    }
+}
---
+++ src/main/java/com/shopwell/api/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.shopwell.api.repository;
+
+import com.shopwell.api.model.entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+}
